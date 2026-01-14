72f25242f8aa7f0f60e5357917f4f50a210c5365
windwireless2026 <wind.wireless2022@gmail.com>
2026-01-13 20:40:10 -0500

    Adjust estimate sequence to 2283

--- supabase/migrations/079_adjust_estimate_sequence.sql
+++ supabase/migrations/079_adjust_estimate_sequence.sql
@@ -1,4 +1,10 @@
--- Adjust estimate number sequence to start from 2283 as requested
-ALTER SEQUENCE estimates_estimate_number_seq RESTART WITH 2283;
+-- Adjust estimate sequence to start from 2283 as requested by user
+-- This ensures the next created estimate will have the number 2283
 
+SELECT setval('estimates_estimate_number_seq', 2282, false);
+
+-- If the above fails due to permissions in some environments, we can also use:
+-- ALTER SEQUENCE estimates_estimate_number_seq RESTART WITH 2283;
+
+-- Reload schema cache
 NOTIFY pgrst, 'reload schema';
